48a990921eb56b962bf9db642439706d52e0573c
kiti-sites <103646253+kiti-sites@users.noreply.github.com>
2025-01-16 10:53:36 -0300

    Update doors.lua

--- doors.lua
+++ doors.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/kiti-sites/GRS/refs/heads/main/rayfield.lua'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "Gui Roblox Scripts",
